d79384aeec8a9af56a3a033bab4853e041ef6e7b
Charles <charles.capiaux@compufirst.com>
2021-10-22 16:54:38 +0200

    script incident

---
+++ BDD/fill_incident.sql
@@ -0,0 +1,21 @@
+INSERT INTO `omni_vision`.`incident`
+(`id_incident`,
+`nom_incident`,
+`description_incident`,
+`gravite_incident`)
+VALUES
+(null,
+"Départ de feu",
+"Départ de feu sur la facade droite qui a été rapidement maitrisé",
+4);
+
+INSERT INTO `omni_vision`.`incident`
+(`id_incident`,
+`nom_incident`,
+`description_incident`,
+`gravite_incident`)
+VALUES
+(null,
+"Carrence en ciment",
+"Manque de ciment",
+1);
