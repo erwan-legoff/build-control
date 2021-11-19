1ed151ad0385a137a81d2bd58dc20ab500af0625
Erwan Le Goff <erwan.legoff@compufirst.com>
2021-11-19 15:22:18 +0100

    Les chantiers sont bien reçus à partir de l'API

--- OmnivisionApplication/app/src/main/java/com/example/omnivisionapplication/ListChantierActivity.java
+++ OmnivisionApplication/app/src/main/java/com/example/omnivisionapplication/ListChantierActivity.java
@@ -1,32 +1,65 @@
 package com.example.omnivisionapplication;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.example.omnivisionapplication.placeholder.Chantier;
 import com.example.omnivisionapplication.placeholder.ChantierAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+
 public class ListChantierActivity extends AppCompatActivity {
 
-
-
+    MethodsOmniVision methodsOmniVision = RetrofitClientOmniVision.getRetrofitInstance().create(MethodsOmniVision.class);
+    Call<ModelOmniVision> call = methodsOmniVision.getAllChantier();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_chantier);
+        ArrayList<ModelOmniVision.Chantier> list = new ArrayList<>();
+        call.enqueue(new Callback<ModelOmniVision>() {
+            @Override
+            public void onResponse(Call<ModelOmniVision> call, retrofit2.Response<ModelOmniVision> responseOmniVision) {
+                Log.e(TAG, "onResponse: AVANT IS EXECUTED  = " + call.isExecuted() );
+                Log.e(TAG, "onResponse: APRES getChantier IS EXECUTED  = " + call.isExecuted() );
+                Log.e("On Response", "LISTE -------------------------> : "+ list.size());
+                List<ModelOmniVision.Chantier> listChantier = responseOmniVision.body().getChantiers();
+                if(listChantier == null){
+                    Log.e(TAG, "onResponse: ERREUR la liste chantier est null" );
+                }else{
+                    Log.e(TAG, "onResponse: LA LISTE CHANTIER N'EST PAS null YOUHOU !!! sa taille est de " + listChantier.size() );
+                    createListView(listChantier);
+                }
 
-        List<Chantier> listChantier = getListData();
-        final ListView listView = (ListView) findViewById(R.id.listViewChantier);
+            }
+
+            @Override
+            public void onFailure(Call<ModelOmniVision> call, Throwable t) {
+                Log.e(TAG, "onFailure: ERREUR FAILURE" + t.getMessage()  );
+            }
+
+
+        });
+
+
+
+    }
+
+    private void createListView(List<ModelOmniVision.Chantier> listChantier) {
+        final ListView listView = findViewById(R.id.listViewChantier);
         listView.setAdapter(new ChantierAdapter(this, listChantier));
 
 
@@ -36,23 +69,32 @@ public class ListChantierActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Object o = listView.getItemAtPosition(position);
-                Chantier chantier = (Chantier) o;
+                ModelOmniVision.Chantier chantier = (ModelOmniVision.Chantier) o;
                 Toast.makeText(ListChantierActivity.this, "Selected :" + " " + chantier, Toast.LENGTH_LONG).show();
             }
 
         });
     }
 
-    private  List<Chantier> getListData() {
-        List<Chantier> list = new ArrayList<Chantier>();
-        Chantier enzob = new Chantier(1, "chantier cool", R.drawable.logoouvrier, "chantier_image1");
-        Chantier aymeriche = new Chantier(2, "chantier cool", R.drawable.logoouvrier, "logoouvrier");
-        Chantier leocratie = new Chantier(3, "chantier cool", R.drawable.logoouvrier, "logoouvrier");
+    private  ArrayList<ModelOmniVision.Chantier> getListData() {
 
 
-        list.add(enzob);
-        list.add(aymeriche);
-        list.add(leocratie);
+        ArrayList<ModelOmniVision.Chantier> list = new ArrayList<>();
+        call.enqueue(new Callback<ModelOmniVision>() {
+            @Override
+            public void onResponse(Call<ModelOmniVision> call, retrofit2.Response<ModelOmniVision> responseOmniVision) {
+                Log.e(TAG, "onResponse: AVANT IS EXECUTED  = " + call.isExecuted() );
+                list.addAll(responseOmniVision.body().getChantiers());
+                Log.e(TAG, "onResponse: APRES getChantier IS EXECUTED  = " + call.isExecuted() );
+                Log.e("On Response", "LISTE -------------------------> : "+ list.size());
+            }
+
+            @Override
+            public void onFailure(Call<ModelOmniVision> call, Throwable t) {
+            }
+
+
+        });
 
         return list;
     }
